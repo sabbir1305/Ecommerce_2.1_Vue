3cc7206083da14208a24f8b28bc04aa027fc3aae
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-08-23 18:24:51 +0600

    Add project files.

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+   public class CreateProduct
+    {
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Save(Product product)
+        {
+            _context.Products.Add(product);
+        }
+    }
+}
